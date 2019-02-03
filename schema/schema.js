742f31a5b509c2b41c428fee2e125bf42a35781e
Alexis <alexislui.dev@gmail.com>
2019-02-03 21:20:27 +0000

    4.28: Edit user mutation

--- schema/schema.js
+++ schema/schema.js
@@ -97,6 +97,20 @@ const mutation = new GraphQLObjectType({
           .delete(`${jsonServerEndpoint}/users/${id}`)
           .then(response => response.data)
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`${jsonServerEndpoint}/users/${args.id}`, args)
+          .then(response => response.data)
+      }
     }
   }
 })
