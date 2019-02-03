286ef087f55eda041034331204fc4d6902618471
Alexis <alexislui.dev@gmail.com>
2019-02-03 02:33:58 +0000

    3.11: Prettier. RootQuery. GraphQLSchema

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,11 @@
 const graphql = require('graphql')
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt
-} = graphql
+const _ = require('lodash')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
+
+const users = [
+  { id: 'a', firstName: 'A', age: 1 },
+  { id: 'b', firstName: 'B', age: 2 }
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -12,4 +14,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id })
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
